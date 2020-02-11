b5204f7077138da0ffb35c0db6227ca26934622c
Ryan <imacube@gmail.com>
2020-02-11 11:39:30 -0800

    Refactored into a class

--- sort.py
+++ sort.py
@@ -7,162 +7,161 @@ from datetime import datetime
 from pathlib import Path
 
 
-def parse_arguments():
-    """Parse command line arguments."""
+class SortingPictures:
+    def __init__(self):
+        pass
 
-    parser = argparse.ArgumentParser(
-        description="""Sort image files based on filename timestamp into year-month folders.""")
-    parser.add_argument('--move', action='store_true', required=False, default=False,
-                        help="Move files instead of copying them (default action is to copy).")
-    parser.add_argument('paths', nargs=argparse.REMAINDER, help='source source source ... destination')
+    @staticmethod
+    def parse_arguments():
+        """Parse command line arguments."""
 
-    return parser
+        parser = argparse.ArgumentParser(
+            description="""Sort image files based on filename timestamp into year-month folders.""")
+        parser.add_argument('--move', action='store_true', required=False, default=False,
+                            help="Move files instead of copying them (default action is to copy).")
+        parser.add_argument('paths', nargs=argparse.REMAINDER, help='source source source ... destination')
 
+        return parser
 
-def get_date_from_filename(filename):
-    """Derive the images timestamp from the filename.
+    def get_date_from_filename(self, filename):
+        """Derive the images timestamp from the filename.
 
-    :param filename: Filename of the image file.
-    :return: datetime.datetime
-    """
+        :param filename: Filename of the image file.
+        :return: datetime.datetime
+        """
 
-    f = filename.split('.')[0]
-    f = f.replace('-', '_')
-    f = f.split('_')[1:3]
-    f = ''.join(f)
+        f = filename.split('.')[0]
+        f = f.replace('-', '_')
+        f = f.split('_')[1:3]
+        f = ''.join(f)
 
-    try:
-        return datetime.strptime(f, '%Y%m%d%H%M%S')
-    except ValueError:
-        return None
+        try:
+            return datetime.strptime(f, '%Y%m%d%H%M%S')
+        except ValueError:
+            return None
 
+    @staticmethod
+    def search_directory(sp):
+        """Return the contents of a directory.
 
-def search_directory(sp):
-    """Return the contents of a directory.
+        :param sp: Path to search.
+        :return: list of directory contents.
+        """
 
-    :param sp: Path to search.
-    :return: list of directory contents.
-    """
+        return [x for x in Path(sp).rglob('*')]
 
-    return [x for x in Path(sp).rglob('*')]
+    @staticmethod
+    def is_file(file_path):
+        """Check if the path is a file or something else.
 
+        :param file_path: Path to check.
+        :return: True if it is a file, False otherwise.
+        """
 
-def is_file(file_path):
-    """Check if the path is a file or something else.
+        p = Path(file_path)
 
-    :param file_path: Path to check.
-    :return: True if it is a file, False otherwise.
-    """
-
-    p = Path(file_path)
-
-    if p.is_symlink():
-        return False
-    elif p.is_file():
-        return True
-    elif p.is_dir():
-        return False
-    else:
-        return False
-
-
-def diff_files(src_file, dest_file):
-    """Hash two files and see if they are the same or not.
-
-    :param src_file: Source file.
-    :param dest_file: Destination file.
-    :return:
-    """
-    # return run_hash(path_to_file, hashlib.sha512()).hexdigest()
-
-    src_hash, dest_hash = hashlib.sha512(), hashlib.sha512()
-
-    with open(src_file, 'rb') as file_in:
-        for block in iter(lambda: file_in.read(4096), b''):
-            src_hash.update(block)
-
-    with open(dest_file, 'rb') as file_in:
-        for block in iter(lambda: file_in.read(4096), b''):
-            dest_hash.update(block)
-
-    return src_hash.hexdigest() == dest_hash.hexdigest()
-
-
-def move_file(src_file, dest_file, move=False):
-    """Move a file from the src to the dest.
-
-    :param src_file: Source path.
-    :param dest_file: Destination path.
-    :param move: True to move files, False to copy them.
-    :return: None
-    """
-
-    src = Path(src_file)
-    dest = Path(dest_file)
-
-    dest.parent.mkdir(parents=True, exist_ok=True)
-
-    if is_file(dest) and not diff_files(src, dest):
-        return False
-
-    if move:
-        shutil.move(src, dest)
-    else:
-        shutil.copy2(src, dest)
-
-    return True
-
-
-def sort_images(src_path, dest_path, move=False):
-    """Sort files from the source path into the destination path.
-
-    :param src_path: Path to read the files from.
-    :param dest_path: Path to write files to.
-    :param move: True to move files, False to copy them.
-    :return:
-    """
-
-    for src in search_directory(src_path):
-        if not is_file(src):
-            continue
-        d = get_date_from_filename(src.name)
-        if d is None:
-            continue
-
-        if src.suffix.lower() in ['.jpg', '.png']:
-            prefix = 'IMG_'
-        elif src.suffix.lower() in ['.mp4']:
-            prefix = 'VID_'
+        if p.is_symlink():
+            return False
+        elif p.is_file():
+            return True
+        elif p.is_dir():
+            return False
         else:
-            continue
+            return False
 
-        dest = dest_path / d.strftime('%Y-%m') / (prefix + d.strftime('%Y%m%d_%H%M%S') + src.suffix)
+    def diff_files(self, src_file, dest_file):
+        """Hash two files and see if they are the same or not.
 
-        move_file(src, dest, move)
+        :param src_file: Source file.
+        :param dest_file: Destination file.
+        :return:
+        """
+        # return run_hash(path_to_file, hashlib.sha512()).hexdigest()
 
+        src_hash, dest_hash = hashlib.sha512(), hashlib.sha512()
 
-def main(args):
-    """Main method to be called by CLI.
+        with open(src_file, 'rb') as file_in:
+            for block in iter(lambda: file_in.read(4096), b''):
+                src_hash.update(block)
 
-    :param args: CLI arguments.
-    :return:
-    """
+        with open(dest_file, 'rb') as file_in:
+            for block in iter(lambda: file_in.read(4096), b''):
+                dest_hash.update(block)
 
-    if '--move' in args.paths:
-        print('--move must come before source and destination paths.')
-        sys.exit(1)
+        return src_hash.hexdigest() == dest_hash.hexdigest()
 
-    dest_path = Path(args.paths[-1])
+    def move_file(self, src_file, dest_file, move=False):
+        """Move a file from the src to the dest.
 
-    for src_path in [Path(p) for p in args.paths[:-1]]:
-        sort_images(src_path, dest_path, move=args.move)
+        :param src_file: Source path.
+        :param dest_file: Destination path.
+        :param move: True to move files, False to copy them.
+        :return: None
+        """
+
+        src = Path(src_file)
+        dest = Path(dest_file)
+
+        dest.parent.mkdir(parents=True, exist_ok=True)
+
+        if self.is_file(dest) and not self.diff_files(src, dest):
+            return False
+
+        if move:
+            shutil.move(src, dest)
+        else:
+            shutil.copy2(src, dest)
+
+        return True
+
+    def sort_images(self, src_path, dest_path, move=False):
+        """Sort files from the source path into the destination path.
+
+        :param src_path: Path to read the files from.
+        :param dest_path: Path to write files to.
+        :param move: True to move files, False to copy them.
+        :return:
+        """
+
+        for src in self.search_directory(src_path):
+            if not self.is_file(src):
+                continue
+            d = self.get_date_from_filename(src.name)
+            if d is None:
+                continue
+
+            if src.suffix.lower() in ['.jpg', '.png']:
+                prefix = 'IMG_'
+            elif src.suffix.lower() in ['.mp4']:
+                prefix = 'VID_'
+            else:
+                continue
+
+            dest = dest_path / d.strftime('%Y-%m') / (prefix + d.strftime('%Y%m%d_%H%M%S') + src.suffix)
+
+            self.move_file(src, dest, move)
+
+    def main(self):
+        """Main method to be called by CLI.
+
+        :return:
+        """
+
+        parser = self.parse_arguments()
+        args = parser.parse_args()
+        if len(args.paths) < 2:
+            parser.print_help()
+            sys.exit(1)
+
+        if '--move' in args.paths:
+            print('--move must come before source and destination paths.')
+            sys.exit(1)
+
+        dest_path = Path(args.paths[-1])
+
+        for src_path in [Path(p) for p in args.paths[:-1]]:
+            self.sort_images(src_path, dest_path, move=args.move)
 
 
 if __name__ == '__main__':
-    parse = parse_arguments()
-    args = parse.parse_args()
-    if not len(args.paths):
-        parse.print_help()
-        sys.exit(1)
-
-    main(args)
+    SortingPictures().main()
--- test_sort.py
+++ test_sort.py
@@ -3,83 +3,88 @@ import shutil
 from argparse import Namespace
 from datetime import datetime
 from pathlib import PosixPath
-from unittest.mock import patch, call
+from unittest.mock import patch, call, MagicMock
 
-from sort import get_date_from_filename, search_directory, is_file, move_file, sort_images, diff_files, parse_arguments, \
-    main
+import pytest
+
+from sort import SortingPictures
+
+
+@pytest.fixture
+def sorting_pictures():
+    return SortingPictures()
 
 
 class TestParseArguments:
-    def test_basic(self):
-        parser = parse_arguments()
+    def test_basic(self, sorting_pictures):
+        parser = sorting_pictures.parse_arguments()
         args = parser.parse_args('src dest'.split())
         assert args == Namespace(move=False, paths=['src', 'dest'])
 
-    def test_move(self):
-        parser = parse_arguments()
+    def test_move(self, sorting_pictures):
+        parser = sorting_pictures.parse_arguments()
         args = parser.parse_args('--move src dest'.split())
         assert args == Namespace(move=True, paths=['src', 'dest'])
 
-    def test_many_sources(self):
-        parser = parse_arguments()
+    def test_many_sources(self, sorting_pictures):
+        parser = sorting_pictures.parse_arguments()
         args = parser.parse_args('src0 src1 src2 src3 dest'.split())
         assert args == Namespace(move=False, paths=['src0', 'src1', 'src2', 'src3', 'dest'])
 
 
 class TestGetDateFromFile:
-    def test_image_basic(self):
-        actual = get_date_from_filename('IMG_20171022_124203.jpg')
+    def test_image_basic(self, sorting_pictures):
+        actual = sorting_pictures.get_date_from_filename('IMG_20171022_124203.jpg')
 
         expected = datetime(year=2017, month=10, day=22, hour=12, minute=42, second=3)
 
         assert actual == expected
 
-    def test_image_duplicate(self):
-        actual = get_date_from_filename('IMG_20171104_104157_01.jpg')
+    def test_image_duplicate(self, sorting_pictures):
+        actual = sorting_pictures.get_date_from_filename('IMG_20171104_104157_01.jpg')
 
         expected = datetime(year=2017, month=11, day=4, hour=10, minute=41, second=57)
 
         assert actual == expected
 
-    def test_screenshot(self):
-        actual = get_date_from_filename('Screenshot_20171007-143321.png')
+    def test_screenshot(self, sorting_pictures):
+        actual = sorting_pictures.get_date_from_filename('Screenshot_20171007-143321.png')
 
         expected = datetime(year=2017, month=10, day=7, hour=14, minute=33, second=21)
 
         assert actual == expected
 
-    def test_video(self):
-        actual = get_date_from_filename('VID_20180724_173611.mp4')
+    def test_video(self, sorting_pictures):
+        actual = sorting_pictures.get_date_from_filename('VID_20180724_173611.mp4')
 
         expected = datetime(year=2018, month=7, day=24, hour=17, minute=36, second=11)
 
         assert actual == expected
 
-    def test_failure(self):
-        actual = get_date_from_filename('metadata.jpg')
+    def test_failure(self, sorting_pictures):
+        actual = sorting_pictures.get_date_from_filename('metadata.jpg')
 
         assert actual is None
 
 
 class TestIsFile:
-    def test_file(self):
-        file_path = 'sample-images/metadata.jpg'
-        assert is_file(file_path)
+    def test_file(self, sorting_pictures):
+        assert sorting_pictures.is_file('sample-images/metadata.jpg')
 
-    def test_symlink(self):
-        assert not is_file('sample-images/no-m')
-        assert not is_file('sample-images/VID')
+    def test_symlink(self, sorting_pictures):
+        assert not sorting_pictures.is_file('sample-images/no-m')
+        assert not sorting_pictures.is_file('sample-images/VID')
 
-    def test_dir(self):
-        assert not is_file('sample-images')
+    def test_dir(self, sorting_pictures):
+        assert not sorting_pictures.is_file('sample-images')
 
-    def test_other(self):
-        assert not is_file('/dev/null')
+    def test_other(self, sorting_pictures):
+        assert not sorting_pictures.is_file('/dev/null')
 
 
 class TestSearchDirectory:
-    def test_dir(self):
-        result = search_directory('sample-images')
+    def test_dir(self, sorting_pictures):
+        result = sorting_pictures.search_directory('sample-images')
 
         assert result == [PosixPath('sample-images/metadata-copy.jpg'), PosixPath('sample-images/VID'),
                           PosixPath('sample-images/no-m'), PosixPath('sample-images/no-metadata.jpg'),
@@ -92,15 +97,15 @@ class TestSearchDirectory:
 
 
 class TestDiffFiles:
-    def test_same_hash(self):
-        assert diff_files('sample-images/metadata.jpg', 'sample-images/metadata-copy.jpg') is True
+    def test_same_hash(self, sorting_pictures):
+        assert sorting_pictures.diff_files('sample-images/metadata.jpg', 'sample-images/metadata-copy.jpg') is True
 
-    def test_different_hash(self):
-        assert diff_files('sample-images/metadata.jpg', 'sample-images/no-metadata.jpg') is False
+    def test_different_hash(self, sorting_pictures):
+        assert sorting_pictures.diff_files('sample-images/metadata.jpg', 'sample-images/no-metadata.jpg') is False
 
 
 class TestMoveFile:
-    def test_copy_file(self, tmp_path):
+    def test_copy_file(self, sorting_pictures, tmp_path):
         src = tmp_path / 'src'
         src.mkdir(parents=True, exist_ok=True)
         src_file = src / 'metadata.jpg'
@@ -109,10 +114,10 @@ class TestMoveFile:
         dest_file = tmp_path / 'dest' / 'metadata-dest.jpg'
 
         assert not dest_file.exists()
-        assert move_file(src_file, dest_file) is True
+        assert sorting_pictures.move_file(src_file, dest_file) is True
         assert dest_file.exists()
 
-    def test_move_file(self, tmp_path):
+    def test_move_file(self, sorting_pictures, tmp_path):
         src = tmp_path / 'src'
         src.mkdir(parents=True, exist_ok=True)
         src_file = src / 'metadata.jpg'
@@ -122,11 +127,11 @@ class TestMoveFile:
 
         assert src_file.exists()
         assert not dest_file.exists()
-        assert move_file(src_file, dest_file, move=True) is True
+        assert sorting_pictures.move_file(src_file, dest_file, move=True) is True
         assert not src_file.exists()
         assert dest_file.exists()
 
-    def test_file_exists_same_hash(self, tmp_path):
+    def test_file_exists_same_hash(self, sorting_pictures, tmp_path):
         src = tmp_path / 'src'
         src.mkdir(parents=True, exist_ok=True)
         src_file = src / 'metadata.jpg'
@@ -135,12 +140,12 @@ class TestMoveFile:
         dest_file = tmp_path / 'dest' / 'metadata-dest.jpg'
 
         assert not dest_file.exists()
-        move_file(src_file, dest_file)
+        sorting_pictures.move_file(src_file, dest_file)
         assert dest_file.exists()
 
-        assert move_file(src_file, dest_file) is True
+        assert sorting_pictures.move_file(src_file, dest_file) is True
 
-    def test_file_exists_different_hash(self, tmp_path):
+    def test_file_exists_different_hash(self, sorting_pictures, tmp_path):
         src = tmp_path / 'src'
         src.mkdir(parents=True, exist_ok=True)
         src_file = src / 'metadata.jpg'
@@ -149,17 +154,17 @@ class TestMoveFile:
         dest_file = tmp_path / 'dest' / 'metadata-dest.jpg'
 
         assert not dest_file.exists()
-        move_file(src_file, dest_file)
+        sorting_pictures.move_file(src_file, dest_file)
         assert dest_file.exists()
 
         src_file = src / 'no-metadata.jpg'
         shutil.copy2('sample-images/no-metadata.jpg', src_file)
 
-        assert move_file(src_file, dest_file) is False
+        assert sorting_pictures.move_file(src_file, dest_file) is False
 
 
 class TestSortImages:
-    def test_successful_run_copy(self, tmp_path):
+    def test_successful_run_copy(self, sorting_pictures, tmp_path):
         src = tmp_path / 'src'
         dest = tmp_path / 'dest'
 
@@ -170,9 +175,9 @@ class TestSortImages:
 
         shutil.copytree('sample-images', src, symlinks=True)
 
-        sort_images(src, dest)
+        sorting_pictures.sort_images(src, dest)
 
-        result = search_directory(dest)
+        result = sorting_pictures.search_directory(dest)
         result = [p.relative_to(tmp_path) for p in result]
 
         assert result == [PosixPath('dest/2018-07'), PosixPath('dest/2017-11'), PosixPath('dest/2017-10'),
@@ -181,7 +186,7 @@ class TestSortImages:
                           PosixPath('dest/2017-10/IMG_20171007_143321.png'),
                           PosixPath('dest/2017-10/IMG_20171022_124203.jpg')]
 
-    def test_successful_run_move(self, tmp_path):
+    def test_successful_run_move(self, sorting_pictures, tmp_path):
         src = tmp_path / 'src'
         dest = tmp_path / 'dest'
 
@@ -192,9 +197,9 @@ class TestSortImages:
 
         shutil.copytree('sample-images', src, symlinks=True)
 
-        sort_images(src, dest, move=True)
+        sorting_pictures.sort_images(src, dest, move=True)
 
-        result = search_directory(dest)
+        result = sorting_pictures.search_directory(dest)
         result = [p.relative_to(tmp_path) for p in result]
 
         assert result == [PosixPath('dest/2018-07'), PosixPath('dest/2017-11'), PosixPath('dest/2017-10'),
@@ -203,12 +208,12 @@ class TestSortImages:
                           PosixPath('dest/2017-10/IMG_20171007_143321.png'),
                           PosixPath('dest/2017-10/IMG_20171022_124203.jpg')]
 
-        result = search_directory(src)
+        result = sorting_pictures.search_directory(src)
         result = [p.relative_to(tmp_path) for p in result]
         assert result == [PosixPath('src/metadata-copy.jpg'), PosixPath('src/VID'), PosixPath('src/no-m'),
                           PosixPath('src/no-metadata.jpg'), PosixPath('src/metadata.jpg'), PosixPath('src/no-metadata')]
 
-    def test_unknown_suffix(self, tmp_path):
+    def test_unknown_suffix(self, sorting_pictures, tmp_path):
         src = tmp_path / 'src'
         dest = tmp_path / 'dest'
 
@@ -222,65 +227,69 @@ class TestSortImages:
         bad_suffix = src / 'IMG_20170102_030405.UNKNOWN_FOOBAR'
         bad_suffix.touch()
 
-        sort_images(src, dest)
-        result = search_directory(dest)
+        sorting_pictures.sort_images(src, dest)
+        result = sorting_pictures.search_directory(dest)
 
         assert result == list()
 
 
 class TestMain:
-    @patch('sort.sort_images')
-    def test_basic(self, mock_sort_images):
-        parser = parse_arguments()
-        args = parser.parse_args('src dest'.split())
-        main(args)
+    @patch('sort.SortingPictures.sort_images')
+    @patch('sort.SortingPictures.parse_arguments')
+    def test_basic(self, mock_parser, mock_sort_images, sorting_pictures):
+        mock_parser.return_value.parse_args.return_value = Namespace(move=False, paths='src dest'.split())
+        sorting_pictures.main()
 
         mock_sort_images.assert_called_with(PosixPath('src'), PosixPath('dest'), move=False)
 
-    @patch('sort.sort_images')
-    def test_basic_move(self, mock_sort_images):
-        parser = parse_arguments()
-        args = parser.parse_args('--move src dest'.split())
-        main(args)
+    @patch('sort.SortingPictures.sort_images')
+    @patch('sort.SortingPictures.parse_arguments')
+    def test_basic_move(self, mock_parser, mock_sort_images, sorting_pictures):
+        mock_parser.return_value.parse_args.return_value = Namespace(move=True, paths='src dest'.split())
+        sorting_pictures.main()
 
         mock_sort_images.assert_called_with(PosixPath('src'), PosixPath('dest'), move=True)
 
-    @patch('sort.sort_images')
-    def test_multi_src(self, mock_sort_images):
-        parser = parse_arguments()
-        args = parser.parse_args('src0 src1 src2 dest'.split())
-        main(args)
+    @patch('sort.SortingPictures.sort_images')
+    @patch('sort.SortingPictures.parse_arguments')
+    def test_multi_src(self, mock_parser, mock_sort_images, sorting_pictures):
+        mock_parser.return_value.parse_args.return_value = Namespace(move=False, paths='src0 src1 src2 dest'.split())
+        sorting_pictures.main()
 
         assert mock_sort_images.call_args_list == [call(PosixPath('src0'), PosixPath('dest'), move=False),
                                                    call(PosixPath('src1'), PosixPath('dest'), move=False),
                                                    call(PosixPath('src2'), PosixPath('dest'), move=False)]
 
-    @patch('sort.sort_images')
-    def test_multi_src_move(self, mock_sort_images):
-        parser = parse_arguments()
-        args = parser.parse_args('--move src0 src1 src2 dest'.split())
-        main(args)
+    @patch('sort.SortingPictures.sort_images')
+    @patch('sort.SortingPictures.parse_arguments')
+    def test_multi_src_move(self, mock_parser, mock_sort_images, sorting_pictures):
+        mock_parser.return_value.parse_args.return_value = Namespace(move=True, paths='src0 src1 src2 dest'.split())
+        sorting_pictures.main()
 
         assert mock_sort_images.call_args_list == [call(PosixPath('src0'), PosixPath('dest'), move=True),
                                                    call(PosixPath('src1'), PosixPath('dest'), move=True),
                                                    call(PosixPath('src2'), PosixPath('dest'), move=True)]
 
     @patch('sys.exit')
-    def test_bad_order(self, mock_exit):
-        parser = parse_arguments()
-        args = parser.parse_args('src0 src1 src2 dest --move'.split())
-        main(args)
+    @patch('sort.SortingPictures.parse_arguments')
+    def test_bad_order(self, mock_parser, mock_exit, sorting_pictures):
+        mock_parser.return_value.parse_args.return_value = Namespace(move=False,
+                                                                     paths='src0 src1 src2 dest --move'.split())
+        sorting_pictures.main()
         mock_exit.assert_called_once_with(1)
 
-        args = parser.parse_args('src0 src1 --move src2 dest'.split())
+        mock_parser.return_value.parse_args.return_value = Namespace(move=False,
+                                                                     paths='src0 src1 --move src2 dest'.split())
         mock_exit.reset_mock()
-        main(args)
+        sorting_pictures.main()
         mock_exit.assert_called_once_with(1)
 
-    @patch('sort.sort_images')
-    def test_too_few_args(self, mock_sort_images):
-        parser = parse_arguments()
-        args = parser.parse_args('src'.split())
-        main(args)
+    @patch('sys.exit')
+    @patch('sort.SortingPictures.sort_images')
+    @patch('sort.SortingPictures.parse_arguments')
+    def test_too_few_args(self, mock_parser, mock_sort_images, mock_exit, sorting_pictures):
+        mock_parser.return_value.parse_args.return_value = Namespace(move=False, paths='src'.split())
+        sorting_pictures.main()
 
         mock_sort_images.assert_not_called()
+        mock_exit.assert_called_once_with(1)
